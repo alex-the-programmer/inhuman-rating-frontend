0efcb37fa19d29a02042b8e32c243de6456cf6dd
alex <AlexTheProgrammer@hotmail.com>
2018-09-02 20:58:54 -0700

    Profile card mostly done

---
+++ src/components/cards/Profile/Profile.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import Card from "@material-ui/core/Card/Card";
+import CardContent from "@material-ui/core/CardContent/CardContent";
+import Typography from "@material-ui/core/Typography/Typography";
+import classes from './Profile.css';
+
+const profile = (props) => {
+    return(
+      <Card>
+          <CardContent className={classes.Card}>
+              <div className={classes.Column}>
+                  <Typography variant="headline">
+                      {props.name}
+                  </Typography>
+                  <Typography variant="subheading" color="textSecondary">
+                      <ul className={classes.List}>
+                        {props.cities.map(city=> <li key={city.id}>{city.name}, {city.state.acronym}</li>)}
+                      </ul>
+                  </Typography>
+              </div>
+              <div className={classes.Column}>
+                  <Typography variant="subheading" color="textSecondary">
+                      <ul className={classes.List}>
+                          {props.companies.map(company=> <li key={company.id}>{company.name}</li>)}
+                      </ul>
+                  </Typography>
+              </div>
+              <div className={classes.Column}>
+                  <Typography variant="subheading" color="textSecondary">
+                      <ul className={classes.List}>
+                          {props.schools.map(school=> <li key={school.id}>{school.name}</li>)}
+                      </ul>
+                  </Typography>
+              </div>
+          </CardContent>
+      </Card>
+    );
+};
+
+export default profile;
--- src/containers/Search/Search.js
+++ src/containers/Search/Search.js
@@ -3,6 +3,7 @@ import Textbox from "../../components/forms/Textbox/Textbox";
 import classes from './Search.css';
 import InputAdornmentButton from "../../components/forms/InputAdornmentButton/InputAdornmentButton";
 import SearchIcon from '@material-ui/icons/Search';
+import ProfileCard from "../../components/cards/Profile/Profile";
 
 export default class Search extends Component{
     render(){
@@ -16,6 +17,41 @@ export default class Search extends Component{
                                 <SearchIcon/>
                         </InputAdornmentButton>
                         }/>
+
+
+               <div className={classes.CardsList}>
+
+               </div>
+                <ProfileCard
+                    name="Alex M"
+                    cities={[{
+                        id: 1,
+                        name: 'San Francisco',
+                        state: {
+                            acronym: 'CA'
+                        }
+                    }, {
+                        id: 2,
+                        name: 'Sunnyvale',
+                        state: {
+                            acronym: 'CA'
+                        }
+                    }]}
+                    companies={[{
+                        id: 1,
+                        name: 'TrueCar'
+                    }, {
+                        id: 2,
+                        name: 'Beepi'
+                    }]}
+                    schools={[{
+                        id: 1,
+                        name: 'School 1'
+                    }, {
+                        id: 2,
+                        name: 'School 2'
+                    }]}/>
+
            </div>
         )
     }
