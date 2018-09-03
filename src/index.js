1dd4422ac02a7d26a547754bd6939ac7beda79ef
alex <AlexTheProgrammer@hotmail.com>
2018-09-03 00:01:08 -0700

    redux thunk added

--- src/index.js
+++ src/index.js
@@ -5,10 +5,11 @@ import App from './App';
 import { applyMiddleware, createStore } from 'redux';
 import { Provider } from 'react-redux';
 import logger from 'redux-logger';
+import thunk from 'redux-thunk'
 import registerServiceWorker from './registerServiceWorker';
 
 import reducer from './store/reducer';
-const store = createStore(reducer, applyMiddleware(logger));
+const store = createStore(reducer, applyMiddleware(logger, thunk));
 
 const app = (
     <Provider store={store}>
